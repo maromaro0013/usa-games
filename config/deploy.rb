fe3e53c6037c7e84eabafe00f20842e8c006336e
user <user@administraotr-no-MacBook-Air.local>
2015-12-24 17:38:15 +0900

    rubyのversion設定

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,13 +2,13 @@
 lock '3.4.0'
 
 set :application, 'usa-games'
-set :repo_url, 'https://github.com/maromaro0013/funa-games.git'
+set :repo_url, 'https://github.com/maromaro0013/usa-games.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/home/usa-games/#{fetch(:application)}'
+set :deploy_to, "/home/usa-games/#{fetch(:application)}"
 
 # Default value for :scm is :git
 # set :scm, :git
